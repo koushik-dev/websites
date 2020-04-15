5b45b66481c4d847de404a135671cc5145b2a993
koushik <koushik.ramaswamy@apitive.com>
2020-04-15 21:58:30 +0530

    portfolio changes

--- portfolio/index.js
+++ portfolio/index.js
@@ -1,3 +1,51 @@
-function opener(event) {
-    document.querySelector('.hero').classList.add('active');
+function opener() {
+  gsap
+    .timeline()
+    .add("open")
+    .to(".hero__opener__left", {
+      keyframes: [
+        {
+          width: 0
+        },
+        {
+          display: "none",
+          delay: 1
+        }
+      ]
+    })
+    .to(
+      ".hero__opener__right",
+      {
+        keyframes: [
+          {
+            width: 0
+          },
+          {
+            display: "none",
+            delay: 1
+          }
+        ]
+      },
+      "open"
+    )
+    .to(
+      ".hero__opener__text",
+      {
+        keyframes: [
+          {
+            y: "-100%",
+            duration: 1,
+            opacity: 0
+          },
+          {
+            display: "none",
+            delay: 1
+          }
+        ]
+      },
+      "open"
+    )
+    .to('.hero__opener', {
+        display: 'none'
+    });
 }
