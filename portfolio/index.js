cc4ef8aa4b9549d54dacfeb17f84fd43b310bbc3
koushik <koushik.ramaswamy@apitive.com>
2020-04-18 22:15:47 +0530

    page animations and home page

--- portfolio/index.js
+++ portfolio/index.js
@@ -1,15 +1,47 @@
-function selectedTab(index, fromTop) {
-  const docElem = document.documentElement;
-  let color;
-  docElem.querySelectorAll('.hero__header__links li').forEach((el, i) => {
-    if (i + 1 === index) {
-      el.classList.add('active');
-      color = "--" + el.getAttribute('data-color')
-    } else {
-      el.classList.remove('active');
-    }
-  });
-  fromTop && (docElem.querySelector('.hero__footer__buttons').children[index - 1].querySelector("input").checked = !0);
-  docElem.style.setProperty('--selected-tab', (index * 25) + '%')
-  docElem.style.setProperty('--theme-color', getComputedStyle(docElem).getPropertyValue(color))
+const docElem = document.documentElement;
+let activePage = 1;
+
+function changePage(index, fromTop) {
+  changeSelectedHeader(index);
+  fromTop && changeSelectedFooter(index);
+  changeContent(index);
+  activePage = index;
+}
+
+function changeSelectedHeader(index) {
+  let color,
+    elem = docElem.querySelectorAll(".hero__header__links li")[index - 1];
+  elem.classList.add("active");
+  color = "--" + elem.getAttribute("data-color");
+  docElem
+    .querySelectorAll(".hero__header__links li")
+    [activePage - 1].classList.remove("active");
+  docElem.style.setProperty("--selected-tab", index * 25 + "%");
+  docElem.style.setProperty(
+    "--theme-color",
+    getComputedStyle(docElem).getPropertyValue(color)
+  );
+}
+
+function changeSelectedFooter(index) {
+  docElem
+    .querySelector(".hero__footer__buttons")
+    .children[index - 1].querySelector("input").checked = !0;
+}
+
+function changeContent(index) {
+  let tl = gsap.timeline();
+  tl.to(".hero__section main:nth-child(" + activePage + ")", {
+    duration: 0.5,
+    height: "0%",
+  })
+    .to(".hero__section main:nth-child(" + activePage + ")", {
+      duration: 0,
+      display: "none",
+    })
+    .to(".hero__section main:nth-child(" + index + ")", {
+      duration: 1,
+      display: "grid",
+      height: "100%",
+    });
 }
